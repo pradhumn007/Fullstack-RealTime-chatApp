660127d635bc200acbdab517e5c9c7996d9113e5
Pradhumn singh bhadoria <pradhumn.bhadawar@gmail.com>
2025-10-30 15:21:57 +0530

    Project Completed

---
+++ backend/src/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.routes.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
+
+import path from "path";
+
+// Load environment variables from .env
+dotenv.config();
+
+// Create Express app
+
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+// Increase payload size limits (important fix)
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+// Middleware for cookies and CORS
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// Connect to the database, then start the server
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
---
+++ backend/src/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MongoDB connected successfully:", conn.connection.host);
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
